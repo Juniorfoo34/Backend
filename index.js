0f601e4058e76fca9df868960206329cd37e9ee6
brian loboa <80613821+Juniorfoo34@users.noreply.github.com>
2026-05-27 04:21:40 -0500

    Not listening on correct port

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
